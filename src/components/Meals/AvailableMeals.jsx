0e39f1f2b3c63455f8c0b110b69a8fe610776186
NikaAtanelishvili <atanelishvilinika@gmail.com>
2022-10-03 18:39:08 +0400

    Fetching Meals Data

--- src/components/Meals/AvailableMeals.jsx
+++ src/components/Meals/AvailableMeals.jsx
@@ -1,9 +1,11 @@
 import styles from './AvailableMeals.module.css'
 import MealItem from './MealItem/MealItem'
 import Card from '../UI/Card'
+import { useEffect, useState } from 'react'
+import FIREBASE_API_URL from '../../helper/config'
 
 const DUMMY_MEALS = [
-  {
+  /* {
     id: 'm1',
     name: 'Sushi',
     description: 'Finest fish and veggies',
@@ -26,11 +28,60 @@ const DUMMY_MEALS = [
     name: 'Green Bowl',
     description: 'Healthy...and green...',
     price: 18.99,
-  },
+  }, */
 ]
 
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map(meal => (
+  const [meals, setMeals] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+  const [httpError, setHttpError] = useState()
+
+  useEffect(() => {
+    ;(async () => {
+      const response = await fetch(FIREBASE_API_URL)
+
+      if (!response.ok) throw new Error('⚠️ Something went wrong! ')
+
+      const responseData = await response.json()
+      console.log(responseData)
+      const loadedMeals = []
+
+      for (const key in responseData) {
+        loadedMeals.push({
+          id: key,
+          name: responseData[key].name,
+          description: responseData[key].description,
+          price: responseData[key].price,
+        })
+      }
+
+      setMeals(loadedMeals)
+      setIsLoading(false)
+    })().catch(err => {
+      // handeling an error inside of a promise
+      console.log(err)
+      setIsLoading(false)
+      setHttpError(err.message)
+    })
+  }, [])
+
+  if (isLoading) {
+    return (
+      <section className={styles.measlLoading}>
+        <p>Loading...</p>
+      </section>
+    )
+  }
+
+  if (httpError) {
+    return (
+      <section className={styles.MealsError}>
+        <p>{httpError}</p>
+      </section>
+    )
+  }
+
+  const mealsList = meals.map(meal => (
     <MealItem
       id={meal.id}
       key={meal.id}
---
+++ src/helper/config.jsx
@@ -0,0 +1,4 @@
+const FIREBASE_API_URL =
+  'https://food-order-app-3a36d-default-rtdb.firebaseio.com/meals.json'
+
+export default FIREBASE_API_URL
